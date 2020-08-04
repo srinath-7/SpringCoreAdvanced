3f26d71004ec01c368d4b1f20f6b73d06f25a865
Srinath <gopathisrinath@gmail.com>
2020-08-04 13:54:40 +0530

    autowire by type in xml

---
+++ SpringCoreAdvanced/src/main/java/com/sri/springcore/springcoreadvanced/autowiring/Employee.java
@@ -0,0 +1,23 @@
+package com.sri.springcore.springcoreadvanced.autowiring;
+
+public class Employee {
+	
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+
+
+	
+
+}
