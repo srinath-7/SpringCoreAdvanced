1f2febb389e42056ec3a42afee34c2c6222d0b31
Srinath <gopathisrinath@gmail.com>
2020-08-05 12:14:47 +0530

    Using of the Qualifier annotation

--- SpringCoreAdvanced/src/main/java/com/sri/springcore/springcoreadvanced/autowiring/annotation/Employee.java
+++ SpringCoreAdvanced/src/main/java/com/sri/springcore/springcoreadvanced/autowiring/annotation/Employee.java
@@ -1,27 +1,19 @@
 package com.sri.springcore.springcoreadvanced.autowiring.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Employee {
 	
+	@Autowired(required = false)
+	@Qualifier("address123")
 	private Address address;
 	
-	
-
-	public Employee(Address address) {
-		super();
-		this.address = address;
-	}
-	
-
 	public Address getAddress() {
 		return address;
 	}
     
-	@Autowired
-	public void setAddress(Address address) {
-		this.address = address;
-	}
+
 	
 	@Override
 	public String toString() {
